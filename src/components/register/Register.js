30ad7b7176a8c4f13bb27d312e12e39b806dac38
yojimat <visoccer22@htmail.com>
2018-10-18 13:01:53 -0300

    Adicionando validações e deixando mais leve.

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormErrors } from '../formErrors/formErrors';
 
 class Register extends React.Component {
 	constructor(props) {
@@ -6,21 +7,72 @@ class Register extends React.Component {
 		this.state = {
 			nome: '',
 			email: '',
-			senha: ''
+			senha: '',
+			formErrors: {Email: '', Password: '', Name: ''},
+    		emailValid: false,
+    		passwordValid: false,
+    		nameValid: false, 
+    		formValid: false
 		}
 	}
 
 	onNameChange = (event) => {
-		this.setState({nome: event.target.value});
+		let RegisterNameName = event.target.name;
+		let RegisterNamevalue = event.target.value;
+
+		this.setState({nome: event.target.value}, () => { this.validateField(RegisterNameName, RegisterNamevalue ) });
 	}
 
 	onEmailChange = (event) => {
-		this.setState({email: event.target.value});
+		let RegisterEmailName = event.target.name;
+		let RegisterEmailvalue = event.target.value;
+
+		this.setState({email: event.target.value}, () => { this.validateField(RegisterEmailName, RegisterEmailvalue ) });
 	}
 
 	onPasswordChange = (event) => {
-		this.setState({senha: event.target.value});
+		let RegisterSenhaName = event.target.name;
+		let RegisterSenhavalue = event.target.value;
+
+		this.setState({senha: event.target.value}, () => { this.validateField(RegisterSenhaName, RegisterSenhavalue ) });
 	}
+
+	validateField(fieldName, value) {
+	    let fieldValidationErrors = this.state.formErrors;
+	    let emailValid = this.state.emailValid;
+	    let passwordValid = this.state.passwordValid;
+	    let nameValid = this.state.nameValid;
+
+	    switch(fieldName) {
+	      case 'email-address':
+	        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+	        fieldValidationErrors.Email = emailValid ? '' : ' Email inválido.';
+	        break;
+	      case 'password':
+	        passwordValid = value.length >= 3;
+	        fieldValidationErrors.Password = passwordValid ? '' : ' Senha inválida.';
+	        break;
+	      case 'name':
+	        nameValid = value.length == 0;
+	        fieldValidationErrors.Name = nameValid ? '' : ' Nome inválido.';
+	        break;
+	      default:
+	        break;
+	    }
+	    this.setState({formErrors: fieldValidationErrors,
+	                    emailValid: emailValid,
+	                    passwordValid: passwordValid,
+	                    nameValid: nameValid
+	                  }, this.validateForm);
+    }
+
+    validateForm() {
+    	this.setState({formValid: this.state.emailValid && this.state.passwordValid && this.state.nameValid});
+  	}
+
+  	errorClass(error) {
+    	return(error.length === 0 ? '' : 'tem algum erro.');
+  	}
 
 	onSubmitRegister = () => {
 		console.log(this.state);
@@ -49,33 +101,42 @@ class Register extends React.Component {
 					<div className="measure">
 					    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 					      <legend className="f1 fw6 ph0 mh0">Registro</legend>
-					      <div className="mt3">
+					      <div className="f6 f6-m f6-l fw3 w-100 mt0 lh-copy">
+          					<FormErrors formErrors={this.state.formErrors} />
+          				  </div>
+					      <div className={`mt3 ${this.errorClass(this.state.formErrors.Name)}`}>
 					        <label className="db fw6 lh-copy f6" htmlFor="name">Nome</label>
 					        <input 
 					        	className="pa2 input-reset ba bg-transparent hover-bg-white hover-black w-100 b--white white" 
 					        	type="text" 
 					        	name="name" 
 					        	id="name"
+					        	placeholder="nome"
+					        	value={this.state.nome}
 					        	onChange={this.onNameChange} 
 					        />
 					      </div>
-					      <div className="mt3">
+					      <div className={`mt3 ${this.errorClass(this.state.formErrors.Email)}`}>
 					        <label className="db fw6 lh-copy f6" htmlFor="email-address">E-mail</label>
 					        <input 
 					        	className="pa2 input-reset ba bg-transparent hover-bg-white hover-black w-100 b--white white" 
 					       		type="email" 
 					        	name="email-address"  
 					        	id="email-address"
+					        	placeholder="e-mail"
+					        	value={this.state.email}
 					        	onChange={this.onEmailChange}
 					        />
 					      </div>
-					      <div className="mv3">
+					      <div className={`mv3 ${this.errorClass(this.state.formErrors.Password)}`}>
 					        <label className="db fw6 lh-copy f6" htmlFor="password">Senha</label>
 					        <input 
 					        	className="b pa2 input-reset ba bg-transparent hover-bg-white hover-black w-100 b--white white" 
 					        	type="password" 
 					        	name="password"  
 					        	id="password"
+					        	placeholder="senha"
+					        	value={this.state.senha}
 					        	onChange={this.onPasswordChange}
 					        />
 					      </div>
@@ -85,7 +146,9 @@ class Register extends React.Component {
 					      	className="b ph3 pv2 input-reset ba b--white white bg-transparent grow pointer f6 dib" 
 					      	type="submit" 
 					      	value="Feito!" 
-					      	onClick={this.onSubmitRegister}/>
+					      	onClick={this.onSubmitRegister}
+					      	disabled={!this.state.formValid}
+					      />
 					    </div>
 					</div>
 				</main>
--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -1,22 +1,64 @@
 import React from 'react';
 import './Signin.css';
+import { FormErrors } from '../formErrors/formErrors';
 
 class Signin extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			signInEmail: '',
-			signInPassword: ''
+			signInPassword: '',
+			formErrors: {Email: '', Password: ''},
+    		emailValid: false,
+    		passwordValid: false, 
+    		formValid: false
 		}
 	}
 
 	onEmailChange = (event) => {
-		this.setState({signInEmail: event.target.value});
+		let signInEmailname = event.target.name;
+		let signInEmailvalue = event.target.value;
+
+		this.setState({signInEmail: event.target.value},() => { this.validateField(signInEmailname, signInEmailvalue ) });
 	}
 
 	onPasswordChange = (event) => {
-		this.setState({signInPassword: event.target.value});
+		let signInPasswordname = event.target.name;
+		let signInPasswordvalue = event.target.value;
+
+		this.setState({signInPassword: event.target.value},() => { this.validateField(signInPasswordname,signInPasswordvalue ) });
 	}
+
+	validateField(fieldName, value) {
+	    let fieldValidationErrors = this.state.formErrors;
+	    let emailValid = this.state.emailValid;
+	    let passwordValid = this.state.passwordValid;
+
+	    switch(fieldName) {
+	      case 'signInEmail':
+	        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+	        fieldValidationErrors.Email = emailValid ? '' : ' Email inválido.';
+	        break;
+	      case 'signInPassword':
+	        passwordValid = value.length >= 3;
+	        fieldValidationErrors.Password = passwordValid ? '' : ' Senha inválida.';
+	        break;
+	      default:
+	        break;
+	    }
+	    this.setState({formErrors: fieldValidationErrors,
+	                    emailValid: emailValid,
+	                    passwordValid: passwordValid
+	                  }, this.validateForm);
+    }
+
+    validateForm() {
+    	this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  	}
+
+  	errorClass(error) {
+    	return(error.length === 0 ? '' : 'tem algum erro.');
+  	}
 
 	onSubmitSignIn = () => {
 		fetch('https://frozen-caverns-42300.herokuapp.com/signin', {
@@ -47,32 +89,49 @@ class Signin extends React.Component {
 					<div className="measure">
 					    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 					      <legend className="f1 fw6 ph0 mh0">Acesso</legend>
-					      <div className="mt3">
+					      <div className="f6 f6-m f6-l fw3 w-100 mt0 lh-copy">
+          					<FormErrors formErrors={this.state.formErrors} />
+          				  </div>
+					      <div className={`mt3 ${this.errorClass(this.state.formErrors.Email)}`}>
 					        <label className="db fw6 lh-copy f6" htmlFor="email-address">E-mail</label>
 					        <input  
 					        	className="pa2 input-reset ba bg-transparent hover-bg-white hover-black w-100 b--white white" 
 					        	type="email" 
-					        	name="email-address"  
+					        	name="signInEmail"  
 					        	id="email-address"
+					        	placeholder="e-mail"
+					        	value={this.state.signInEmail}
 					        	onChange={this.onEmailChange}
 					        />
 					      </div>
-					      <div className="mv3">
+					      <div className={`mv3 ${this.errorClass(this.state.formErrors.Password)}`}>
 					        <label className="db fw6 lh-copy f6" htmlFor="password">Senha</label>
 					        <input 
 					        	className="b pa2 input-reset ba bg-transparent hover-bg-white hover-black w-100 b--white white" 
 					        	type="password" 
-					        	name="password"  
+					        	name="signInPassword"  
 					        	id="password"
+					        	placeholder="senha"
+					        	value={this.state.signInPassword}
 					        	onChange={this.onPasswordChange} 
 					        />
 					      </div>
 					    </fieldset>
 					    <div className="">
-					      <input className="b ph3 pv2 input-reset ba b--white white bg-transparent grow pointer f6 dib" type="submit" value="Logue" onClick={this.onSubmitSignIn}/>
+					      <input 
+						      className="b ph3 pv2 input-reset ba b--white white bg-transparent grow pointer f6 dib" 
+						      type="submit" 
+						      value="Logue" 
+						      onClick={this.onSubmitSignIn}
+						      disabled={!this.state.formValid}
+					      />
 					    </div>
 					    <div className="lh-copy mt3">
-					      <p onClick={() => onRouteChange('register')} className="f6 link dim white db pointer">Registre-se</p>
+					      <p 
+					      	onClick={() => onRouteChange('register')} 
+					      	className="f6 link dim white db pointer">
+					      	Registre-se
+					      	</p>
 					    </div>
 					</div>
 				</main>
